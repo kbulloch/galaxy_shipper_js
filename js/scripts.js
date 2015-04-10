99c27b5e7df92f6c6788ac541023fdf11fe8af7b
Kyle Bulloch <student@epicodus.com>
2015-04-10 11:00:19 -0700

    Add jquery that outputs a result at the bottom of the html page

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 var triangler = function(input) {
 
   input.sort(); //sorts input for lowest to highest
-
   if((input[0] + input[1]) <= input[2]){ //checks if a triangle cannot be formed
     return "Invalid Input";
   }
@@ -23,5 +22,34 @@ var triangler = function(input) {
 
 
 $(document).ready(function() {
+
+  $("form#triangle_input").submit(function(event) {
+    var a = parseInt($("input#a").val());
+    var b = parseInt($("input#b").val());
+    var c = parseInt($("input#c").val());
+    console.log("a:" + a);
+
+
+    var outcome = triangler([a, b, c]);
+    console.log(outcome);
+
+    if(outcome === "Equilateral") {
+      $("#output").text("an Equilateral triangle");
+    }
+    if(outcome === "Isosceles") {
+      $("#output").text("an Isosceles triangle");
+    }
+    if(outcome === "Scalene") {
+      $("#output").text("a Scalene triangle");
+    }
+    if(outcome === "Invalid Input") {
+      $("#output").text("not a valid triangle");
+    }
+
+
+
+    $("#result").show();
+    event.preventDefault();
+  });
 
 });
