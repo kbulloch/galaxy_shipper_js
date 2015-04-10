2f0aaf55fa7007db292ae526e706fe5ebb0e1d39
Kyle Bulloch <student@epicodus.com>
2015-04-10 09:35:27 -0700

    Add test for invalid input

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 var triangler = function(input) {
+  if((input[0] + input[1]) <= input[2]){
+    console.log("a: " + input[0]);
+    console.log("b: " + input[1]);
+    console.log("c: " + input[2]);
+    return "Invalid Input";
+  }
   return "Equilateral";
 };
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,11 @@
 describe('triangler', function() {
 
-  it('will return Equilateral for an input of all equal values', function() {
+  it('will return "Equilateral" for an input of all equal values', function() {
     expect(triangler([1, 1, 1])).to.equal("Equilateral");
+  });
+
+  it('will return "Invalid Input" for input values that cannot form a triangle', function() {
+    expect(triangler([1, 1, 3])).to.equal("Invalid Input");
   });
 
 });
